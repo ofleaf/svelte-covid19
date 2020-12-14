e62ab630dd69f97c06391f6fede5d4dd84c60777
shcheon <ofleaf@gmail.com>
2020-12-15 00:16:17 +0900

    Add styles and example chart.js

--- src/App.svelte
+++ src/App.svelte
@@ -9,12 +9,12 @@ let country = 'korea-south';
 let totalStatus = [];
 
 onMount(async () => {
-  fetchCountryByTotalStatus()
-})
+  await fetchCountryByTotalStatus();
+});
 
 function handleChange () {
-  fetchCountryByTotalStatus()
-};
+  fetchCountryByTotalStatus();
+}
 
 async function fetchCountryByTotalStatus() {
   const totalStatusAPI = `https://api.covid19api.com/total/country/${country}`;
@@ -41,43 +41,104 @@ export let title;
 <CovidApi bind:countries={countries}/>
 
 <main>
-  <h1>{title}</h1>
-  <select bind:value={country} on:change={handleChange}>
-  {#each countries as country}
-    <option value="{country.Slug}">{country.Country}</option>
-  {/each}
-  </select>
-  <ChartComponent/>
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Country</th>
-        <th>Date</th>
-        <th>Confirmed</th>
-        <th>Deaths</th>
-        <th>Recovered</th>
-        <th>Active</th>
-      </tr>
-    </thead>
-    <tbody>
-      {#each totalStatus as item}
-      <tr>
-        <td>{item.Country}</td>
-        <td>{item.Date}</td>
-        <td>{item.Confirmed}</td>
-        <td>{item.Deaths}</td>
-        <td>{item.Recovered}</td>
-        <td>{item.Active}</td>
-      </tr>
+  <div class="container">
+    <h1 class="title">{title}</h1>
+    <div class="country-div">
+      <select bind:value={country} on:change={handleChange}>
+      {#each countries as country}
+        <option value="{country.Slug}">{country.Country}</option>
       {/each}
-    </tbody>
-  </table>
+      </select>
+    </div>
+    <ChartComponent/>
+    <table class="status-table">
+      <thead>
+        <tr class="status-table__tr">
+          <th class="status-table__th">Country</th>
+          <th class="status-table__th">Date</th>
+          <th class="status-table__th">Confirmed</th>
+          <th class="status-table__th">Deaths</th>
+          <th class="status-table__th">Recovered</th>
+          <th class="status-table__th">Active</th>
+        </tr>
+      </thead>
+      <tbody>
+        {#each totalStatus as item}
+        <tr class="status-table__tr">
+          <td class="status-table__td">{item.Country}</td>
+          <td class="status-table__td">{item.Date}</td>
+          <td class="status-table__td">{item.Confirmed}</td>
+          <td class="status-table__td">{item.Deaths}</td>
+          <td class="status-table__td">{item.Recovered}</td>
+          <td class="status-table__td">{item.Active}</td>
+        </tr>
+        {/each}
+      </tbody>
+    </table>
+  </div>
 </main>
 
 <style lang="scss">
-  .table {
-    td {
+  @import "./assets/styles/_mixin";
+
+  @include sm {
+    .container {
+      width: 540px;
+    }
+  }
+
+  @include md {
+    .container {
+      width: 720px;
+    }
+  }
+
+  @include lg {
+    .container {
+      width: 960px;
+    }
+  }
+
+  @include xl {
+    .container {
+      width: 1140px;
+    }
+  }
+
+  @include xxl {
+    .container {
+      width: 1320px;
+    }
+  }
+
+  .container {
+    margin: 0 auto;
+    padding: 1rem 0;
+  }
+
+  .title {
+    font-size: 2rem;
+    text-align: center;
+    font-weight: bold;
+  }
+
+  .country-div {
+    text-align: center;
+  }
+
+  .status-table {
+    width: 100%;
+    margin: 0 auto;
+    text-align: center;
+    .status-table__tr {
       border: 1px solid black;
+      .status-table__th {
+        font-weight: bold;
+        padding: .5rem 1rem;
+      }
+      .status-table__td {
+        padding: .25rem 1rem;
+      }
     }
   }
 </style>
